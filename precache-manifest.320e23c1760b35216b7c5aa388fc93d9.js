bb94c7a779ee53c7a056890d7a9d887d8ddca554
Abhishek Singh <newtonabhishek2002@gmal.com>
2024-07-11 18:37:36 +0530

    Updates

---
+++ precache-manifest.320e23c1760b35216b7c5aa388fc93d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb19809e8ba83d98dbecfa140d3b943",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "e47efbe10748861dc162",
+    "url": "/TextUtils-React/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "6ec13f354cb04b791f89",
+    "url": "/TextUtils-React/static/js/2.3cc1542a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.3cc1542a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162ccf896bffc81abd11",
+    "url": "/TextUtils-React/static/js/3.d9878319.chunk.js"
+  },
+  {
+    "revision": "e47efbe10748861dc162",
+    "url": "/TextUtils-React/static/js/main.a6ac6a4b.chunk.js"
+  },
+  {
+    "revision": "e1db1d03a8b9cfa93071",
+    "url": "/TextUtils-React/static/js/runtime-main.bb276db4.js"
+  }
+]);
